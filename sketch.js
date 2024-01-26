2c859b09f53c85f022a6e906314f7f106409f1ad
Falke Lindegaard Hvidtfeldt <falkelh@icloud.com>
2024-01-26 13:16:21 +0100

    added image of ourselves

--- sketch.js
+++ sketch.js
@@ -1,4 +1,16 @@
+let img;
+
+function preload() {
+    img = loadImage("billeder/de_smukke.jpg");
+}
+
 function setup() {
-    createCanvas(100, 100);
-    background(200);
+    w = img.width;
+    h = img.height;
+    createCanvas(w, h);
+}
+
+function draw() {
+    image(img, 0, 0);
+    text("hallo", 0, 0);
 }
