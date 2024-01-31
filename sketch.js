c4ed045d9328cc743b7cc9e7f23b084f20832d1f
Falke Lindegaard Hvidtfeldt <falkelh@icloud.com>
2024-01-31 10:44:58 +0100

    made it work

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let img;
+let pixelSize = 10; // SKal holdes lav for high resolution billeder af hensyn til fart
 
 function preload() {
     img = loadImage("billeder/de_smukke.jpg");
@@ -7,10 +8,30 @@ function preload() {
 function setup() {
     w = img.width;
     h = img.height;
-    createCanvas(w, h);
+    createCanvas(2*w, h);
+    noStroke();
+    img.loadPixels();
 }
 
 function draw() {
-    image(img, 0, 0);
-    text("hallo", 0, 0);
+    image(img, w, 0);
+    myFilter();
+    noLoop();
+}
+
+function myFilter() {
+    console.log("Hallo")
+    for (let i = 0; i < w; i += pixelSize) {
+        for (let j = 0; j < h; j+= pixelSize) {
+            console.log("test1");
+            fill(getPixelValue(0,i,j),getPixelValue(1,i,j),getPixelValue(2,i,j));
+            rect(i, j, pixelSize);
+        }
+    }
+}
+
+function getPixelValue(n,i,j){
+    p = img.pixels[(i+w*j)*4+n];
+    console.log(p);
+    return p;
 }
